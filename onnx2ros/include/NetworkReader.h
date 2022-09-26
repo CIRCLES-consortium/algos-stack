e8e4dcced4ccc17b21f26bea95d3b6b46a9485ac
Jonathan Sprinkle <sprinkjm@email.arizona.edu>
2022-09-26 16:46:32 -0500

    next version needed to publish files correctly

--- onnx2ros/include/NetworkReader.h
+++ onnx2ros/include/NetworkReader.h
@@ -30,7 +30,9 @@ class BaseReader{
 
  public:
   BaseReader(ros::NodeHandle *nh, std::string onnx_model);
-
+  int getTargetSpeedFromTensor(std::vector<float> onnxResult);
+  int getTargetGapSettingFromTensor(std::vector<float> onnxResult);
+  
   std::vector<float> forward(std::vector<float> input_values);
 };
 
