a0d19d7c7be4e065a417623e68715cc53a02a3de
Jonathan Sprinkle <jonathan.sprinkle@vanderbilt.edu>
2022-09-27 03:05:47 +0100

    Corrections to ensure compile is working

--- onnx2ros/src/NetworkReader.cpp
+++ onnx2ros/src/NetworkReader.cpp
@@ -15,22 +15,26 @@ BaseReader::BaseReader(ros::NodeHandle *nh, std::string onnx_model):
   nh->setParam("SPACEGAP_SCALE", 1.0);
 }
 
-double BaseReader::forward(std::vector<float> input_values) {
+std::vector<float> BaseReader::forward(std::vector<float> input_values) {
   std::vector<Ort::Value> input_tensors;
+  std::vector<float> result;
   input_tensors.push_back(Ort::Experimental::Value::CreateTensor<float>(
       input_values.data(), input_values.size(), input_shapes[0]));
   auto output_tensors = session.Run(input_names, input_tensors, output_names);
   // work to do to fix the output values to what we want
   const auto *output_values = output_tensors[0].GetTensorData<float>();
   std::cout << "Received result: ";
-  for( std::vector<float>::iterator it=output_values.begin(); it != output_values.end(); it++ )
+  // HACK HACK HACK
+  for( int i=0; i<64; i++ )
   {
-      float val = *it;
+      float val=0;
+      val = output_values[i];
+      result.push_back(val);
       std::cout << val << ", ";
   }
   std::cout << std::endl;
   ROS_INFO("%.8f %.8f %.8f > %.8f", input_values[0], input_values[1], input_values[2], output_values[0]);
-  return output_values[0];
+  return result;
 }
 
 // don't forget to credit Nathan for writing this pseudocode
@@ -38,11 +42,11 @@ int BaseReader::getTargetGapSettingFromTensor(std::vector<float> speedTensors)
 {
        // find argmax of gap setting logits (indexes 61 to 64 excluded)
        int gap_action = 0;
-       float max_gap_logit = logits[0];
+       float max_gap_logit = speedTensors[0];
        for (int i = 61; i < 64; ++i) {
-           if (logits[i] > max_gap_logit) {
+           if (speedTensors[i] > max_gap_logit) {
                gap_action = i;
-               max_gap_logit = logits[i];
+               max_gap_logit = speedTensors[i];
            }
        }
     
@@ -51,7 +55,6 @@ int BaseReader::getTargetGapSettingFromTensor(std::vector<float> speedTensors)
        int gap_setting = gap_action + 1;
 
        return gap_setting;
-   }
 }
 
 int BaseReader::getTargetSpeedFromTensor(std::vector<float> speedTensors)
@@ -71,11 +74,11 @@ int BaseReader::getTargetSpeedFromTensor(std::vector<float> speedTensors)
     */
        // find argmax of speed setting logits (indexes 0 to 61 excluded)
        int speed_action = 0;
-       float max_speed_logit = logits[0];
+       float max_speed_logit = speedTensors[0];
        for (int i = 1; i < 61; ++i) {
-           if (logits[i] > max_speed_logit) {
+           if (speedTensors[i] > max_speed_logit) {
                speed_action = i;
-               max_speed_logit = logits[i];
+               max_speed_logit = speedTensors[i];
            }
        }
     
@@ -84,12 +87,11 @@ int BaseReader::getTargetSpeedFromTensor(std::vector<float> speedTensors)
        int speed_setting = speed_action + 20;
 
        return speed_setting;
-   }
 }
 
 SynchronousReader::SynchronousReader(ros::NodeHandle *nh, std::string onnx_model):
     BaseReader(nh, std::move(onnx_model)){
-  pub = nh->advertise<geometry_msgs::TwistStamped>("v_des", 10);
+  pub_speed = nh->advertise<geometry_msgs::TwistStamped>("v_des", 10);
   sub_v.subscribe(*nh, "vel", 10);
   sub_lv.subscribe(*nh, "lead_vel", 10);
   sub_sg.subscribe(*nh, "space_gap", 10);
@@ -101,7 +103,7 @@ SynchronousReader::SynchronousReader(ros::NodeHandle *nh, std::string onnx_model
 void SynchronousReader::callback(const geometry_msgs::TwistStampedConstPtr& v_msg,
                                  const geometry_msgs::TwistStampedConstPtr& lv_msg,
                                  const geometry_msgs::TwistStampedConstPtr& sg_msg) {
-  float speed_scale, headway_scale;
+  float speed_scale, spacegap_scale;
   nh->getParam("SPEED_SCALE", speed_scale);
   nh->getParam("SPACEGAP_SCALE", spacegap_scale);
   float v = (float) v_msg->twist.linear.x / speed_scale;
@@ -113,9 +115,14 @@ void SynchronousReader::callback(const geometry_msgs::TwistStampedConstPtr& v_ms
   input_values[1] = lv;
   input_values[2] = sg;
 
-  geometry_msgs::TwistStamped delta_v;
-  delta_v.twist.linear.x = SynchronousReader::forward(input_values);
-  pub.publish(delta_v);
+  std_msgs::Int16 target_gap_setting;
+  std_msgs::Int16 target_speed_setting;
+  std::vector<float> result = SynchronousReader::forward(input_values);
+  target_gap_setting.data = SynchronousReader::getTargetGapSettingFromTensor(result);
+  target_speed_setting.data = SynchronousReader::getTargetSpeedFromTensor(result);
+  pub_gap.publish(target_gap_setting);
+  pub_speed.publish(target_speed_setting);
+
 }
 
 PromptReader::PromptReader(ros::NodeHandle *nh, std::string onnx_model):
@@ -134,15 +141,16 @@ void PromptReader::callback_lv(const geometry_msgs::Twist& lv_msg) {state_lv = l
 void PromptReader::callback_sg(const std_msgs::Float64& sg_msg) {state_sg = sg_msg;}
 
 void PromptReader::publish() {
-  float speed_scale, headway_scale;
+  float speed_scale, spacegap_scale;
   float lead_vehicle_history[10];
   nh->getParam("SPEED_SCALE", speed_scale);
   nh->getParam("SPACEGAP_SCALE", spacegap_scale);
   float v = (float) state_v.linear.x / speed_scale;
   float lv = (float) state_lv.linear.x / speed_scale;
   float sg = (float) state_sg.data / spacegap_scale;
-  for( std::vector<double>::iterator it=state_lead_vehicle_history.begin(), int i=0; 
-          it<state_lead_vehicle_history.end(); 
+  int i=0;
+  for( std::vector<double>::iterator it=state_lead_vehicle_history.begin(); 
+          it != state_lead_vehicle_history.end(); 
           it++, i++ )
   {
       lead_vehicle_history[i] = *it;
@@ -160,9 +168,10 @@ void PromptReader::publish() {
 
   std_msgs::Int16 target_gap_setting;
   std_msgs::Int16 target_speed_setting;
-  std::vector<int> result = PromptReader::forward(input_values);
+  std::vector<float> result = PromptReader::forward(input_values);
   target_gap_setting.data = PromptReader::getTargetGapSettingFromTensor(result);
   target_speed_setting.data = PromptReader::getTargetSpeedFromTensor(result);
   pub_gap.publish(target_gap_setting);
   pub_speed.publish(target_speed_setting);
 }
+
